d66b6ce33e2c9fdcb668be136123f6b799cc3339
Mobileshoppos <khoshi19@gmail.com>
2026-05-28 10:22:28 +0500

    Add files via upload

--- footer.js
+++ footer.js
@@ -10,7 +10,7 @@ const footerHTML = `
                 <a href="#" aria-label="Twitter" class="text-gray-400 hover:text-white transition-colors">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"/></svg>
                 </a>
-                <a href="#" aria-label="YouTube" class="text-gray-400 hover:text-white transition-colors">
+                <a href="https://www.youtube.com/@sadapos" aria-label="YouTube" class="text-gray-400 hover:text-white transition-colors">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 4-8 4z"/></svg>
                 </a>
                 <a href="https://www.instagram.com/sadaposofficial/" aria-label="Instagram" class="text-gray-400 hover:text-white transition-colors">
